0e74ef0e9e6293f4038d91da4fa4fcc7f800a340
kvmhyvmvn <97383799+kvmhyvmvn@users.noreply.github.com>
2023-06-21 18:14:55 +0900

    2번 기능_김혜민

--- Dutch_Pay/app/src/main/java/com/example/dutch_pay/MainActivity.java
+++ Dutch_Pay/app/src/main/java/com/example/dutch_pay/MainActivity.java
@@ -14,11 +14,10 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText edt_money;
-    TextView tv_person,tv_money;
-    Button btn_calculate,btn_plus,btn_minus,btn_reset;
+    TextView tv_person, tv_money;
+    Button btn_calculate, btn_plus, btn_minus, btn_reset;
 
-
-    int person,money;
+    int person, money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +33,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_minus = findViewById(R.id.btn_minus);
         btn_reset = findViewById(R.id.btn_reset);
 
-
-
-
         btn_calculate.setOnClickListener(this);
         btn_plus.setOnClickListener(this);
         btn_minus.setOnClickListener(this);
         btn_reset.setOnClickListener(this);
 
-
-
     }
 
     @Override
     public void onClick(View v) {
-
+        if (v.getId() == R.id.btn_plus) {
+            tv_person.setText(++person + "");
+        } else if (v.getId() == R.id.btn_minus) {
+            if (person <= 1) {
+                Toast.makeText(this, "최소 1명 이상 있어야합니다.", Toast.LENGTH_SHORT).show();
+            } else {
+                tv_person.setText(--person + "");
+            }
+        }
     }
 }
